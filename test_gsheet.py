062424073ec23100d87db786eab30f2b6169e708
Jodel <26493812+Gold-pig@users.noreply.github.com>
2022-12-07 00:32:07 +0100

    test number

--- test_gsheet.py
+++ test_gsheet.py
@@ -17,14 +17,15 @@ def run_query(query):
 # sheet_url = 'https://docs.google.com/spreadsheets/d/1Vj3lhdCPzZWvKXzA2RDRugx8wckHyp7u65maLDhSsPY/edit#gid=0'
 
 # rows = run_query(f'SELECT * FROM "{sheet_url}"')
-def search(name):
+def search(a1):
     sheet_url = 'https://docs.google.com/spreadsheets/d/1Vj3lhdCPzZWvKXzA2RDRugx8wckHyp7u65maLDhSsPY/edit#gid=0'
-    a1 = str('{}'.format(name))
-    rows = run_query(f'SELECT * FROM "{sheet_url}" WHERE name = "{a1}"')
+    # a1 = str('{}'.format(name))
+    rows = run_query(f'SELECT * FROM "{sheet_url}" WHERE name = {a1}')
     # Print results.
+    print(rows)
     for row in rows:
         st.write(f"{row.name} bag is :{row.status}:")
-
+    # return rows
 whichcasno = st.text_input('Enter CAS number', value = '', max_chars = None, key = None, type = 'default', help = 'CAS号形如1336-21-6')
 st.write(whichcasno,'Service Staus is following:')
 query_test = search(whichcasno)
