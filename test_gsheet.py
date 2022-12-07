2b103fb48f9e5e51626072d857b6a994e2e97f6a
Jodel <26493812+Gold-pig@users.noreply.github.com>
2022-12-07 01:40:51 +0100

    lower and upper to lower word

--- test_gsheet.py
+++ test_gsheet.py
@@ -21,15 +21,22 @@ def search(a1,a2):
     sheet_url = 'https://docs.google.com/spreadsheets/d/1Vj3lhdCPzZWvKXzA2RDRugx8wckHyp7u65maLDhSsPY/edit#gid=0'
     # a1 = str('{}'.format(name))
     a1 = '{}'.format(a1)
+    a1 = a1.lower()
     a2 = '{}'.format(a2)
+    a2 = a2.lower()
 
     # rows = run_query(f'SELECT * FROM "{sheet_url}" WHERE name = "{a1}"')
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
     # Print results.
     print(rows)
+    n = 0
     for row in rows:
         if f"{row.name}" == a1 and f"{row.num}" == a2:
             st.write(f"{row.name} bag is :{row.status}:")
+            n+=1
+    if n == 0:
+        st.write(f"Not found")
+
     # return rows
 whichcasno1 = st.text_input('Enter First Name', value = '', max_chars = None, key = 1, type = 'default', help = 'Alice')
 whichcasno2 = st.text_input('Enter Order Num', value = '', max_chars = None, key = 2, type = 'default', help = 'R101')
