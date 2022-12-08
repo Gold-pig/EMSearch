5104b04754531da625e1a1103a8006a4efea6310
Jodel <26493812+Gold-pig@users.noreply.github.com>
2022-12-08 01:43:45 +0100

    blank area
    st.write(f"{row.name} bag is: {row.status}:")

--- test_gsheetPrivate.py
+++ test_gsheetPrivate.py
@@ -38,7 +38,7 @@ def search(a1,a2):
     n = 0
     for row in rows:
         if f"{row.name}" == a1 and f"{row.num}" == a2:
-            st.write(f"{row.name} bag is :{row.status}:")
+            st.write(f"{row.name} bag is: {row.status}:")
             n+=1
     if n == 0:
         st.write(f"Not found")
