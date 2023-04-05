c83e4646b217819da2e411fbffdcc5183c576d47
Jodel <26493812+Gold-pig@users.noreply.github.com>
2023-04-06 01:58:12 +0200

    Update test_gsheetPrivate.py
    change ttl from 600 to 6

--- test_gsheetPrivate.py
+++ test_gsheetPrivate.py
@@ -16,7 +16,7 @@ credentials = service_account.Credentials.from_service_account_info(
     ],
 )
 conn = connect(credentials=credentials)
-@st.cache_resource(ttl=600)
+@st.cache_resource(ttl=6)
 
 def run_query(query):
     rows = conn.execute(query, headers=1)
